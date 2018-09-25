024b9c6c93598e5fac0f460040c5cf3ef0fedfdd
Akash Kava <39438041+ackava@users.noreply.github.com>
2018-09-25 09:01:21 +0530

    Added support for literal values

---
+++ EF.Core.Bulk.Tests/UnitTest1.cs
@@ -0,0 +1,59 @@
+using EF.Core.Bulk.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace EF.Core.Bulk.Tests
+{
+    public class UnitTest1: BaseTest
+    {
+        public UnitTest1(ITestOutputHelper writer) : base(writer)
+        {
+
+        }
+
+        [Fact]
+        public async Task BulkInsert()
+        {
+            using (var db = this.CreateContext()) {
+
+                var googleProducts = db.Products.Where(p => p.Name.StartsWith("google"));
+
+                int count = await db.InsertAsync(googleProducts.Select(p => new Product {
+                    Name = p.Name + " V2"
+                }));
+
+                Assert.Equal(2, count);
+
+                count = await db.Products.CountAsync();
+
+                Assert.Equal(6, count);
+
+                count = await db.UpdateAsync(googleProducts.Select(p => new Product {
+                    Archived = true
+                }));
+
+                Assert.Equal(4, count);
+
+                count = await db.Products.Where(x => x.Archived == true).CountAsync();
+
+                Assert.Equal(4, count);
+
+                count = await db.DeleteAsync(db.Products.Where(x => x.Archived == true));
+
+                Assert.Equal(4, count);
+
+                count = await db.Products.Where(x => x.Archived == true).CountAsync();
+
+                Assert.Equal(0, count);
+
+                count = await db.Products.CountAsync();
+
+                Assert.Equal(2, count);
+            }
+        }
+    }
+}
